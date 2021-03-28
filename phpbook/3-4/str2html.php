928acd0c7748d1559e1964015c1d6320cec06fe2
tatsuo48 <green.americanidiot.yokoyama@gmail.com>
2021-03-28 14:34:35 +0900

    finish 3-4

---
+++ phpbook/3-4/str2html.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'functions.php';
+$fp = fopen('bookdata.csv', 'r');
+if ($fp === false) {
+  echo 'ファイルのオープンに失敗しました。';
+  exit;
+}
+while ($row = fgetcsv($fp)) {
+  echo "書籍名" . str2html($row[0]) . "<br>";
+  echo "著者名" . str2html($row[4]) . "<br><br>";
+}
