e401eae99e3dc323c5d017cf944ca2c12c620ed9
tatsuo48 <green.americanidiot.yokoyama@gmail.com>
2021-03-30 09:04:00 +0900

    finish 5-2

---
+++ phpbook/5-2/list.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . '/../3-4/functions.php';
+try {
+  $user = 'root';
+  $password = 'test';
+  $opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
+  ];
+  $dbh = new PDO('mysql:host=mysql;dbname=sample_db', $user, $password, $opt);
+  $sql = 'SELECT title, author FROM books';
+  $statement = $dbh->query($sql);
+
+  while ($row = $statement->fetch()) {
+    echo '書籍名' . str2html($row[0]) . '<br>';
+    echo '著者名' . str2html($row[1]) . '<br><br>';
+  }
+} catch (PDOException $e) {
+  echo "エラー発生！：" . $e->getMessage() . "<br>";
+  exit;
+}
